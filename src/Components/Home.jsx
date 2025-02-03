1d9e7f191957ae79f127e6024ce8f944b3b9d83e
MrJoseV <jc.venegas000@gmail.com>
2025-02-03 00:11:28 -0300

    arreglo de precio en home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
   return (
     <>
     <div className="bodyHome">
-      <CardPizza url='https://img.freepik.com/foto-gratis/rebanadas-pizza-pepperoni-sobre-mesa_140725-809.jpg' alt="pizza española" nombre="Española"  ingredientes={["mozzarella ", "gorgonzola ", "parmesano ", "provolone"] } precio={6950}/>
-      <CardPizza url='https://img.freepik.com/foto-gratis/pizza-salchicha-servida-hierbas-secas_140725-1219.jpg' alt="pizza pepperoni"  nombre="Pepperoni" price={6950} ingredientes={["mozzarella ", "pepperoni ", "orégano "]}/> 
+      <CardPizza url='https://img.freepik.com/foto-gratis/rebanadas-pizza-pepperoni-sobre-mesa_140725-809.jpg' alt="pizza española" nombre="Española" precio={6950} ingredientes={["mozzarella ", "gorgonzola ", "parmesano ", "provolone"] } />
+      <CardPizza url='https://img.freepik.com/foto-gratis/pizza-salchicha-servida-hierbas-secas_140725-1219.jpg' alt="pizza pepperoni"  nombre="Pepperoni" precio={6950} ingredientes={["mozzarella ", "pepperoni ", "orégano "]}/> 
       <CardPizza url='https://img.freepik.com/foto-gratis/pizza-margarita-mesa_140725-1201.jpg' alt="pizza napolitana" nombre="Napolitana" precio={5950} ingredientes={["mozzarella ", "tomates ", "jamón ", "orégano"]}/> 
     
     </div>
